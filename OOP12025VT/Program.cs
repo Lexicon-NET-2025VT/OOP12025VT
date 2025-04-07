6bfa93cda8f9624a44d0b735f61a458305e24dae
Pontus Wittenmark <pontus.wittenmark@gmail.com>
2025-04-07 13:03:13 +0200

    Add project files.

---
+++ OOP12025VT/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace OOP12025VT;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Animal animal = new Animal(10.0);
+    }
+}
+
+class Animal
+{
+    public Animal(double weight)
+    {
+        Weight = weight;
+    }
+
+    public double Weight { get; }
+}
