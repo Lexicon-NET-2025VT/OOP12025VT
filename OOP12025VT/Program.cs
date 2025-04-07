759d9a487678f353e0b5b57ec73bdb917816d77b
Pontus Wittenmark <pontus.wittenmark@gmail.com>
2025-04-07 15:09:10 +0200

    Added polymorphic members

--- OOP12025VT/Program.cs
+++ OOP12025VT/Program.cs
@@ -4,18 +4,75 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var cat = new Cat();
+        var dog = new Dog(10.0);
 
-        Animal animal = new Animal(10.0);
+        var animals = new List<Animal> { cat, dog };
+        foreach (var animal in animals)
+        {
+            var lunch = new Dog(1);
+            animal.Eat(lunch);
+            Console.WriteLine(animal);
+        }
     }
 }
 
-class Animal
+abstract class Animal
 {
+    public double Weight { get; set; }
+
     public Animal(double weight)
     {
         Weight = weight;
+        MakeSound();
     }
 
-    public double Weight { get; }
+    public virtual void Eat(Animal lunch)
+    {
+        Weight += lunch.Weight;
+    }
+
+    public override string ToString()
+    {
+        return $"Weight: {Weight}";
+    }
+
+    public abstract void MakeSound();
+}
+
+class Cat : Animal
+{
+    public double PurrVolume { get; }
+
+    public Cat() : base(4.5)
+    {
+        PurrVolume = Weight * 0.5;
+    }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}, PurrVolume: {PurrVolume}";
+    }
+
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow!");
+    }
+}
+
+class Dog : Animal
+{
+    public Dog(double weight) : base(weight)
+    {
+    }
+
+    public override void Eat(Animal lunch)
+    {
+        Weight += lunch.Weight * 0.8;
+    }
+
+    public override void MakeSound()
+    {
+        Console.WriteLine("Woff!");
+    }
 }
